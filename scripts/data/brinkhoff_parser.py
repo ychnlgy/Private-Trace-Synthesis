3434b0848e7954d49d8f3ac21482f05067066d86
Yuchen Li <ychnlgy@gmail.com>
2019-12-11 14:51:42 -0500

    Added Brinkhoff parser

---
+++ scripts/data/brinkhoff_parser.py
@@ -0,0 +1,44 @@
+import numpy
+
+
+def iter_trajectories(fpath):
+    """Iterate array of shape (T, 2).
+
+    T is the number of points in the trajectory.
+    2 is the number of coordinate axes, x and y.
+
+    To access the 4th (x, y) in the trajectory, do arr[3].
+    To access the y coordinates of the trajectory, do arr[:, 1].
+    """
+    with open(fpath, "r") as f:
+        for i, line in enumerate(f):
+            line = line.rstrip()
+            if _is_trajectory_line(i):
+                yield _check_trajectory(line)
+            else:
+                _check_id(line)
+
+        assert _is_trajectory_line(i)
+
+
+def _is_trajectory_line(i):
+    return i % 2
+
+
+def _check_id(line):
+    assert line[0] == "#"
+    assert line[-1] == ":"
+    assert line[1:-1].isdigit()
+
+
+def _check_trajectory(line):
+    assert line[:3] == ">0:"
+    lines = line[3:].rstrip(";")
+    pieces = [p.split(",") for p in lines.split(";")]
+    coords = [[float(x), float(y)] for x, y in pieces]
+    return numpy.array(coords)
+
+
+if __name__ == "__main__":
+    trajs = list(iter_trajectories("brinkhoff.dat"))
+    assert len(trajs) == 20000
---
+++ scripts/data/plot_trajectories.py
@@ -0,0 +1,34 @@
+import random
+
+import tqdm
+
+from matplotlib import pyplot
+
+
+def plot_trajectories(trajectories, save_path, prob=0.05):
+    """Randomly plots <prob> trajectories.
+
+    The full set of trajectories is messy and takes too long.
+    """
+    trajectories = list(trajectories)
+    for traj in tqdm.tqdm(trajectories, ncols=80):
+        if random.random() < prob:
+            pyplot.plot(traj[:, 0], traj[:, 1], ":")
+    pyplot.savefig(save_path, bbox_inches="tight")
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", required=True)
+    parser.add_argument("--save", default="brinkhoff.png")
+
+    args = parser.parse_args()
+
+    from brinkhoff_parser import iter_trajectories
+    plot_trajectories(
+        iter_trajectories(args.file),
+        args.save
+    )
