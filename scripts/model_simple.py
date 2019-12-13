c745c1e0b1bb260d5e2cdf58ad357896df0b8dba
Yuchen Li <ychnlgy@gmail.com>
2019-12-13 15:31:01 -0500

    Using more convolutions in simple models

--- scripts/model_simple.py
+++ scripts/model_simple.py
@@ -15,15 +15,19 @@ class Generator(torch.nn.Module):
 
     def __init__(self, noise_size, hidden_size, max_traj_len):
         super().__init__()
-
+        assert max_traj_length % 4 == 0
         self.net = torch.nn.Sequential(
-            torch.nn.Linear(noise_size, hidden_size * max_traj_len),
+            torch.nn.Linear(noise_size, hidden_size * max_traj_len//4),
 
-            Lambda(lambda X: X.view(-1, hidden_size, max_traj_len)),
+            Lambda(lambda X: X.view(-1, hidden_size, max_traj_len//4)),
 
             torch.nn.BatchNorm1d(hidden_size),
             torch.nn.ReLU(),
-            torch.nn.Conv1d(hidden_size, hidden_size, 7, padding=3),
+            torch.nn.ConvTranspose1d(hidden_size, hidden_size, 8, stride=2, padding=3),
+
+            torch.nn.BatchNorm1d(hidden_size),
+            torch.nn.ReLU(),
+            torch.nn.ConvTranspose1d(hidden_size, hidden_size, 8, stride=2, padding=3),
 
             torch.nn.BatchNorm1d(hidden_size),
             torch.nn.ReLU(),
@@ -40,21 +44,27 @@ class Discriminator(torch.nn.Module):
 
     def __init__(self, array_length, hidden_size):
         super().__init__()
+        assert array_length % 8 == 0
         self.net = torch.nn.Sequential(
             Lambda(lambda X: X.squeeze(1)),
             torch.nn.utils.spectral_norm(
-                torch.nn.Conv1d(3, hidden_size, 7, padding=3)
+                torch.nn.Conv1d(3, hidden_size, 7, stride=2, padding=3)
             ),
 
             torch.nn.LeakyReLU(),
             torch.nn.utils.spectral_norm(
-                torch.nn.Conv1d(hidden_size, hidden_size, 7, padding=3)
+                torch.nn.Conv1d(hidden_size, hidden_size, 7, stride=2, padding=3)
             ),
 
-            Lambda(lambda X: X.view(-1, hidden_size * array_length)),
             torch.nn.LeakyReLU(),
             torch.nn.utils.spectral_norm(
-                torch.nn.Linear(hidden_size * array_length, 1)
+                torch.nn.Conv1d(hidden_size, hidden_size, 7, stride=2, padding=3)
+            ),
+
+            Lambda(lambda X: X.view(-1, hidden_size * array_length//8)),
+            torch.nn.LeakyReLU(),
+            torch.nn.utils.spectral_norm(
+                torch.nn.Linear(hidden_size * array_length//8, 1)
             )
         )
 
