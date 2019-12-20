7b2ca0e185e8a18db62eb98c4d1ef5db4053b195
Yuchen Li <ychnlgy@utoronto.ca>
2019-12-20 00:01:52 -0500

    D and G for poly, not checked yet

---
+++ scripts/poly_model_simple.py
@@ -0,0 +1,91 @@
+import torch
+
+
+class Lambda(torch.nn.Module):
+
+    def __init__(self, f):
+        super().__init__()
+        self.f = f
+
+    def forward(self, X):
+        return self.f(X)
+
+
+class ResBlock(torch.nn.Module):
+
+    def __init__(self, shortcut, act, layers):
+        super().__init__()
+        self.sht = shortcut
+        self.act = act
+        self.net = torch.nn.Sequential(*layers)
+
+    def forward(self, X):
+        return self.act(self.sht(X) + self.net(X))
+
+
+class Generator(torch.nn.Module):
+
+    def __init__(self, noise_size, hidden_size):
+        super().__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(noise_size, hidden_size),
+			torch.nn.BatchNorm1d(hidden_size)
+			torch.nn.ReLU(),
+
+			torch.nn.Linear(hidden_size, 2 * hidden_size), 
+			torch.nn.BatchNorm1d(2 * hidden_size)
+			torch.nn.ReLU(),
+			
+			torch.nn.Linear(2 * hidden_size, 4 * hidden_size), 
+			torch.nn.BatchNorm1d(4 * hidden_size)
+			torch.nn.ReLU(),
+
+			torch.nn.Linear(4 * hidden_size, out_dim), 
+			torch.nn.BatchNorm1d(out_dim)
+			torch.nn.ReLU(),
+
+            torch.nn.Tanh()
+        )
+
+    def forward(self, z):
+        return self.net(z)
+
+
+class Discriminator(torch.nn.Module):
+
+    def __init__(self, array_length, hidden_size):
+        super().__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.utils.spectral_norm(
+				torch.nn.Linear(array_length, hidden_size)
+            ),
+
+
+			torch.nn.utils.spectral_norm(
+				torch.nn.Linear(hidden_size, 2 * hidden_size)
+			)
+			torch.nn.LeakyReLU(),
+
+			torch.nn.utils.spectral_norm(
+				torch.nn.Linear(2* hidden_size, 4 * hidden_size)
+			)
+			torch.nn.LeakyReLU(),
+
+			torch.nn.utils.spectral_norm(
+				torch.nn.Linear(4 * hidden_size, hidden_size)
+			)
+			torch.nn.LeakyReLU(),
+
+
+			torch.nn.utils.spectral_norm(
+				torch.nn.Linear(hidden_size, 1)
+			)
+		)
+        
+
+    def forward(self, X):
+        return self.net(X)
+
+    @staticmethod
+    def loss(D, X, Xh):
+        return D(X).mean(axis=0) - D(Xh).mean(axis=0)
--- scripts/train.py
+++ scripts/train.py
@@ -1,4 +1,5 @@
 import os
+import pdb
 
 import numpy
 import torch
@@ -8,7 +9,8 @@ import tqdm
 from data import brinkhoff_parser, plot_trajectories
 import model_simple as model
 import model_tiny
-
+import poly_model_simple
+import numpy as np
 
 MID_X = 12000
 MAX_X = 15000
@@ -21,153 +23,161 @@ MAX_TRAJ_LENGTH = 240
 DATASET_SIZE = 20000
 
 
+class poly_dataset(torch.utils.data.Dataset):
+	def __init__(self, fpath):
+		self.data = np.load(fpath)
+	def __len__(self):
+		return len(self.data)
+	def __getitem__(self, idx):
+		if torch.is_tensor(idx):
+			idx = idx.tolist()
+		return self.data[idx]
+
 def create_dataset(
-    fpath,
-    batch_size,
-    max_length
+	fpath,
+	batch_size,
+	max_length
 ):
-    tdata = create_tensorset(fpath, max_length)
-    dload = torch.utils.data.DataLoader(
-        tdata,
-        batch_size=batch_size,
-        shuffle=True
-    )
-    return dload
+	tdata = poly_dataset(fpath)
+	dload = torch.utils.data.DataLoader(
+		tdata,
+		batch_size=batch_size,
+		shuffle=True
+	)
+	return dload
 
 
 def create_tensorset(fpath, max_length):
-    dataset = numpy.zeros(
-        (DATASET_SIZE, 3, max_length),
-        dtype=numpy.float32
-    )
+	dataset = numpy.zeros(
+		(DATASET_SIZE, 3, max_length),
+		dtype=numpy.float32
+	)
 
-    for i, traj in enumerate(brinkhoff_parser.iter_trajectories(fpath)):
-        n = len(traj)
-        dataset[i, 0, :n] = (traj.T[0] - MID_X) / MAX_X
-        dataset[i, 1, :n] = (traj.T[1] - MID_Y) / MAX_Y
-        dataset[i, 2, :n] = +0.5
-        dataset[i, 2, n:] = -0.5
+	for i, traj in enumerate(brinkhoff_parser.iter_trajectories(fpath)):
+		n = len(traj)
+		dataset[i, 0, :n] = (traj.T[0] - MID_X) / MAX_X
+		dataset[i, 1, :n] = (traj.T[1] - MID_Y) / MAX_Y
+		dataset[i, 2, :n] = +0.5
+		dataset[i, 2, n:] = -0.5
 
-    adata = torch.from_numpy(dataset).unsqueeze(1)
-    assert (adata.abs() <= 1.0).all()
-    tdata = torch.utils.data.TensorDataset(adata)
-    return tdata
+	adata = torch.from_numpy(dataset).unsqueeze(1)
+	assert (adata.abs() <= 1.0).all()
+	tdata = torch.utils.data.TensorDataset(adata)
+	return tdata
 
 
 def iter_valid_trajectories(Xh):
-    Xh = Xh.squeeze(1).cpu().numpy()
-    for row in Xh:
-        idx = numpy.argmax(row[2] < 0)
-        out = row[:2, :idx].T
-        yield out
+	Xh = Xh.squeeze(1).cpu().numpy()
+	for row in Xh:
+		idx = numpy.argmax(row[2] < 0)
+		out = row[:2, :idx].T
+		yield out
 
 def plot_and_save(Xh, save_path):
-    Xh = iter_valid_trajectories(Xh)
-    plot_trajectories.plot_trajectories(Xh, save_path, prob=1.0, use_tqdm=True)
+	Xh = iter_valid_trajectories(Xh)
+	plot_trajectories.plot_trajectories(Xh, save_path, prob=1.0, use_tqdm=True)
 
 def train(
-    fpath, batch_size, noise_size,
-    hidden_size, epochs, n_critic,
-    D_lr, G_lr,
-    epoch_sample_cycle,
-    epoch_sample_count,
-    save_path,
-    debug,
-    tiny
+	fpath, batch_size, noise_size,
+	hidden_size, epochs, n_critic,
+	D_lr, G_lr,
+	epoch_sample_cycle,
+	epoch_sample_count,
+	save_path,
+	debug,
+	tiny
 ):
-    global model
+	global model
 
-    if tiny:
-        model = model_tiny
+	model = poly_model_simple 
 
+	if not os.path.isdir(save_path):
+		os.makedirs(save_path)
 
-    if not os.path.isdir(save_path):
-        os.makedirs(save_path)
+	modo_path = os.path.join(save_path, "E%05d.pkl")
+	save_path = os.path.join(save_path, "E%05d.png")
 
-    modo_path = os.path.join(save_path, "E%05d.pkl")
-    save_path = os.path.join(save_path, "E%05d.png")
+	if debug:
+		dset = create_dataset(fpath, epoch_sample_count, MAX_TRAJ_LENGTH)
+		(X,) = next(iter(dset))
+		plot_and_save(X, save_path % 0)
+		return
 
-    if debug:
-        dset = create_dataset(fpath, epoch_sample_count, MAX_TRAJ_LENGTH)
-        (X,) = next(iter(dset))
-        plot_and_save(X, save_path % 0)
-        return
+	dset = create_dataset(fpath, batch_size, MAX_TRAJ_LENGTH)
 
-    dset = create_dataset(fpath, batch_size, MAX_TRAJ_LENGTH)
+	device = ["cpu", "cuda"][torch.cuda.is_available()]
+	print("Using: %s" % device)
 
-    device = ["cpu", "cuda"][torch.cuda.is_available()]
-    print("Using: %s" % device)
+	G = model.Generator(noise_size, hidden_size, MAX_TRAJ_LENGTH).to(device)
+	D = model.Discriminator(MAX_TRAJ_LENGTH, hidden_size).to(device)
 
-    G = model.Generator(noise_size, hidden_size, MAX_TRAJ_LENGTH).to(device)
-    D = model.Discriminator(MAX_TRAJ_LENGTH, hidden_size).to(device)
+	if device == "cuda":
+		D = torch.nn.DataParallel(D)
 
-    if device == "cuda":
-        D = torch.nn.DataParallel(D)
+	D_optim = torch.optim.Adam(D.parameters(), lr=D_lr, betas=(0, 0.999))
+	G_optim = torch.optim.Adam(G.parameters(), lr=G_lr, betas=(0, 0.999))
 
-    D_optim = torch.optim.Adam(D.parameters(), lr=D_lr, betas=(0, 0.999))
-    G_optim = torch.optim.Adam(G.parameters(), lr=G_lr, betas=(0, 0.999))
+	for epoch in range(0, epochs + 1):
 
-    for epoch in range(0, epochs + 1):
+		with tqdm.tqdm(dset, ncols=80) as bar:
+			for i, X in enumerate(bar, 1):
+				pdb.set_trace()
+				X = X.to(device)
+				z = torch.randn(batch_size, noise_size).to(device)
 
-        with tqdm.tqdm(dset, ncols=80) as bar:
-            for i, (X,) in enumerate(bar, 1):
+				if i % n_critic == 0:
 
-                X = X.to(device)
-                z = torch.randn(batch_size, noise_size).to(device)
+					G.train()
 
-                if i % n_critic == 0:
+					D.eval()
+					G_optim.zero_grad()
+					(-D(G(z))).mean().backward()
+					G_optim.step()
 
-                    G.train()
+				else:
+					with torch.no_grad():
+						G.eval()
+						Xh = G(z)
 
-                    D.eval()
-                    G_optim.zero_grad()
-                    (-D(G(z))).mean().backward()
-                    G_optim.step()
+					D.train()
+					loss = D(Xh).mean() - D(X).mean()
+					D_optim.zero_grad()
+					loss.backward()
+					D_optim.step()
 
-                else:
-                    with torch.no_grad():
-                        G.eval()
-                        Xh = G(z)
+					bar.set_description("[E%05d] %.4f" % (epoch, loss.item()))
 
-                    D.train()
-                    loss = D(Xh).mean() - D(X).mean()
-                    D_optim.zero_grad()
-                    loss.backward()
-                    D_optim.step()
+			if epoch % epoch_sample_cycle == 0 or epoch == epochs:
 
-                    bar.set_description("[E%05d] %.4f" % (epoch, loss.item()))
+				with torch.no_grad():
+					G.eval()
+					z = torch.randn(epoch_sample_count, noise_size).to(device)
+					Xh = G(z)
+					plot_and_save(Xh, save_path % epoch)
 
-            if epoch % epoch_sample_cycle == 0 or epoch == epochs:
-
-                with torch.no_grad():
-                    G.eval()
-                    z = torch.randn(epoch_sample_count, noise_size).to(device)
-                    Xh = G(z)
-                    plot_and_save(Xh, save_path % epoch)
-
-                torch.save(G.state_dict(), modo_path % epoch)
+				torch.save(G.state_dict(), modo_path % epoch)
 
 
 if __name__ == "__main__":
 
-    import argparse
+	import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--fpath", required=True)
-    parser.add_argument("--epochs", type=int, default=20000)
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--fpath", required=True)
+	parser.add_argument("--epochs", type=int, default=20000)
 
-    parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--noise_size", type=int, default=32)
-    parser.add_argument("--hidden_size", type=int, default=32)
-    parser.add_argument("--n_critic", type=int, default=3)
-    parser.add_argument("--D_lr", type=float, default=2e-4)
-    parser.add_argument("--G_lr", type=float, default=5e-5)
-    parser.add_argument("--epoch_sample_cycle", type=int, default=20)
-    parser.add_argument("--epoch_sample_count", type=int, default=100)
-    parser.add_argument("--save_path", required=True)
-    parser.add_argument("--debug", type=int, default=0)
-    parser.add_argument("--tiny", type=int, required=True)
+	parser.add_argument("--batch_size", type=int, default=128)
+	parser.add_argument("--noise_size", type=int, default=32)
+	parser.add_argument("--hidden_size", type=int, default=32)
+	parser.add_argument("--n_critic", type=int, default=3)
+	parser.add_argument("--D_lr", type=float, default=2e-4)
+	parser.add_argument("--G_lr", type=float, default=5e-5)
+	parser.add_argument("--epoch_sample_cycle", type=int, default=20)
+	parser.add_argument("--epoch_sample_count", type=int, default=100)
+	parser.add_argument("--save_path", required=True)
+	parser.add_argument("--debug", type=int, default=0)
+	parser.add_argument("--tiny", type=int, required=True)
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    train(**vars(args))
+	train(**vars(args))
